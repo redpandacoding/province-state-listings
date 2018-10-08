e9d5f5a25e7290442abf0c3e5480b8a5e4901337
Jordan Wamser <jwamser@redpandacoding.com>
2018-10-08 08:51:10 -0500

    Initial Commit

---
+++ src/CanadaProvinces.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * @Author: Jordan Wamser <jwamser@redpandacoding.com>
+ * @CreateAt: 10/8/18 7:50 AM
+ */
+
+namespace RedPandaCoding;
+
+
+class CanadaProvinces
+{
+    public function getProvinces()
+    {
+        return [
+            0 => 'Alberta',
+            1 => 'British Columbia',
+            2 => 'Manitoba',
+            3 => 'New Brunswick',
+            4 => 'Newfoundland and Labrador',
+            5 => 'Northwest Territories',
+            6 => 'Nova Scotia',
+            7 => 'Nunavut',
+            8 => 'Ontario',
+            9 => 'Prince Edward Island',
+            10 => 'Québec',
+            11 => 'Saskatchewan',
+            12 => 'Yukon'
+        ];
+    }
+}
